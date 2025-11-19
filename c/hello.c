18337384e83250dd19c57aa8d8ee84363ce1a99f
SOUP1023 <144521843+soup1023dev@users.noreply.github.com>
2025-11-19 13:24:05 +0900

    Update hello.c

--- c/hello.c
+++ c/hello.c
@@ -3,3 +3,13 @@ int main(void){
   printf("hello world\n")
   return 0
 }
+
+#include <efi.h>
+#include <efilib.h>
+
+EFI_STATUS efi_main(EFI_HANDLE img, EFI_SYSTEM_TABLE *systab) {
+    InitializeLib(img, systab);
+    uefi_call_wrapper(systab->ConOut->OutputString, 2, systab->ConOut, L"Hello World!\n\r");
+    return EFI_SUCCESS;
+}
+# 多分これ動きません、特殊すぎて
