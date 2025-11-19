c90d2d53c8c164884f4276dc779df45e7b5a5410
SOUP1023 <144521843+soup1023dev@users.noreply.github.com>
2025-11-19 09:15:51 +0900

    Update hello.py

--- python/hello.py
+++ python/hello.py
@@ -2,6 +2,7 @@
 # ゆっくり内容を理解してみてね！
 import random
 import matplotlib.pyplot as plt
+import pandas as pd
 a = "h"
 b = "e"
 c = "l"
@@ -14,3 +15,4 @@ print(a+b+c+c+d,e+d+f+c+g)
 print(bytes.fromhex("68656c6c6f20776f726c64").decode("utf-8"))
 print(''.join([next(c for c in (chr(random.randint(0,255)) for _ in iter(int,1)) if ord(c)==ord(t)) for t in "hello world"]))
 plt.figure(figsize=(8,3)); plt.plot([0,0,0,0.5,0.5,0.5], [0,1,0.5,0.5,1,0], 'b', linewidth=3); plt.plot([1,1,1.5,1,1,1.5,1,1,1.5], [0,1,1,1,0.5,0.5,0.5,0,0], 'b', linewidth=3); plt.plot([2,2,2.5], [1,0,0], 'b', linewidth=3); plt.plot([3,3,3.5], [1,0,0], 'b', linewidth=3); plt.plot([4,4.5,4.5,4,4,4],[1,1,0,0,0,1], 'b', linewidth=3); plt.axis('equal'); plt.axis('on'); plt.show()
+print(pd.DataFrame({'hello': ['hello', 'hello', 'hello'], 'world': ['world', 'world', 'world']}))
